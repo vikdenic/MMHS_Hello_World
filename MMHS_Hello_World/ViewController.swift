c805e97cd9eb93ec1432dd134f0095375df3147d
vikdenic <vik.denic@gmail.com>
2014-07-22 10:40:16 -0500

    Implemented bang operator to adhere to beta 4

--- MMHS_Hello_World/ViewController.swift
+++ MMHS_Hello_World/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
                             
-    @IBOutlet var myLabel: UILabel
+    @IBOutlet var myLabel: UILabel!
     var favoriteBand = String()
     var numberOfMembers = 4
 
